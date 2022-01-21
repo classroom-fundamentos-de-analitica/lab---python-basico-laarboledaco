426f5a98285b7fe4f13f526856fde4cfa088dc27
laarboledaco <laarboledaco@unal.edu.co>
2022-01-20 19:59:40 -0500

    test9

--- preguntas.py
+++ preguntas.py
@@ -53,18 +53,17 @@ def pregunta_02():
     data = [row[0] for row in data]
     conjuntoUnico = set(data)
     diccionario = {}
-    respuesta = "[\n"
+    respuesta = []
     for clave in conjuntoUnico:
         diccionario[clave] = 0
     for row in data:
         diccionario[row] += 1
     clavesOrdenadas = sorted(diccionario)
     for clave in clavesOrdenadas:
-        respuesta += '("'+clave + '", ' + str(diccionario[clave])+'),\n'
-
-        #respuesta.append('("'+clave + '",' + str(diccionario[clave])+')')
+        #respuesta += '("'+clave + '", ' + str(diccionario[clave])+'),\n'
+        respuesta.append(clave + "," + str(diccionario[clave]))
     #print(respuesta)
-    respuesta += "]"
+
     print(respuesta)
     return respuesta
 
